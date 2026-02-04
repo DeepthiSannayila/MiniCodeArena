94a4ffd52d697a5ec991ac94d022979687567817
Deepthi <deepthisannayila@gmail.com>
2026-02-04 18:13:34 +0530

    Added automatic DB initialization

--- app.py
+++ app.py
@@ -9,6 +9,23 @@ from datetime import datetime
 from flask import session, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 
+def init_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+init_db()
 DB_PATH = "database.db"
 
 # ================= HELPERS =================
